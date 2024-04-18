98cacedc43fd8668664097294e77905dad6a82b6
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-18 17:23:35 -0300

    criado exercicio 3 e seleção de serviço

--- exercicio 1/questao3.py
+++ exercicio 1/questao3.py
@@ -1,3 +1,4 @@
+# print com as boas vindas e serviços
 print('Bem vindo a Copiadora do Gabriel Lopes dos Santos')
 print()
 print('Entre com o tipo de serviço desejado')
@@ -7,4 +8,14 @@ print('IPB - Impressão Preto e Branco')
 print('FOT - Fotocópia')
 print()
 
-Servico = input('Selecione o serviço: ').upper()
+# função para seleção do serviço, upper evita case sensive e else retorna um erro caso seja escolha invalida
+def Seleção_servico():
+    while True:
+        Servico = input('Selecione o serviço: ').upper()
+        if Servico in ['DIG', 'ICO', 'IPB', 'FOT']:
+            return Servico
+        else:
+            print('Escolha inválida, entre com o tipo do serviço novamente')
+
+
+
