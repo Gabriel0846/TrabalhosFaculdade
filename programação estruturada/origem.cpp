b80bc290fb4868472ab4c4a4b24533e0177a57e9
Gabriel Lopes <144557326+Gabriel0846@users.noreply.github.com>
2024-08-24 07:57:46 -0300

    ajustes

--- programação estruturada/origem.cpp
+++ programação estruturada/origem.cpp
@@ -4,5 +4,14 @@
 int main() (
 	setlocale(LC_ALL, "Portuguese");
 
+	int x;
+	printf("Digite um valor para testarmos: \n");
+	scanf_s("%i", &X);
+
+	if (x % 2 == 0) { printf("O numero é par!"); }
+	else {
+		printf("O numero é impar!")
+	}
+
 	returne 0;
 )
