cffadc5cbf3abd87f7309b261b550cfb6c86e66c
Gabriel Lopes <144557326+Gabriel0846@users.noreply.github.com>
2026-07-30 14:52:49 +0000

    revisão programação estruturada

--- programação estruturada/origem.cpp
+++ programação estruturada/origem.cpp
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main() (
-	setlocale(LC_ALL, "Portuguese");
-
-	int x;
-	printf("Digite um valor para testarmos: \n");
-	scanf_s("%i", &X);
-
-	if (x % 2 == 0) { printf("O numero � par!"); }
-	else {
-		printf("O numero � impar!")
-	}
-
-	returne 0;
-);
+int main() {
+	printf("ola mundo!");
+	
+	return 0;
+};
