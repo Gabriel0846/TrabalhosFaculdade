d5be05beb9097344cbb61451d6ba594715559e82
Gabriel Lopes <144557326+Gabriel0846@users.noreply.github.com>
2026-08-05 12:03:17 +0000

    final aula5

--- ProgramacaooEstruturada/aula5/exemplo10.cpp
+++ ProgramacaooEstruturada/aula5/exemplo10.cpp
@@ -13,10 +13,18 @@ void permutar(char *string, int inicio, int fim){
     if (inicio == fim) //condicao de parada
     printf("%s\n", string);
     else {
-        for (int i = inicio; i < fim; i++){
+        for (int i = inicio; i <= fim; i++){
             trocar((string + inicio), (string + i));
-            permutar(string)
+            permutar(string, inicio + 1, fim);
+            trocar((string + inicio), (string + i)); //reverter a troca
         }
-        
     }
+}
+
+int main(){
+    char str[] = "ABC";
+    int tamanho = strlen(str);
+    printf("todas as permutações de %s são: \n", str);
+    permutar(str, 0, tamanho - 1);
+    return 0;
 }
